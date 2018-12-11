0b9aa371e793bb9ec3bbfb9ad44ebbaf8677a833
Andrew Colin Kissa <andrew@topdog.za.net>
2018-12-11 11:42:12 +0200

    TST: Add more tests

--- spoolfile_test.go
+++ spoolfile_test.go
@@ -29,13 +29,15 @@ func init() {
 }
 
 func TestBasics(t *testing.T) {
-	var p, id, hf, df, did, hid, user, sender string
-	var uid, gid int
+	var p, id, hf, df, did, hid, user, sender, heloname string
+	var uid, gid, rcvd, wc int
 	var err error
 	var msg *Msg
 	uid = 93
 	gid = 93
+	rcvd = 1515239630
 	user = "exim"
+	heloname = "-helo_name alcazar.home.topdog-software.com"
 	sender = "andrew@kudusoft.home.topdog-software.com"
 	id = "1eXn2s-0008DG-EX"
 	p = path.Join(gopath, "src/github.com/baruwa-enterprise/go-exim/testdata")
@@ -68,11 +70,49 @@ func TestBasics(t *testing.T) {
 		t.Errorf("Got %q want %q", msg.Sender, sender)
 	}
 
+	if msg.Received != rcvd {
+		t.Errorf("Got %d want %d", msg.Received, rcvd)
+	}
+
+	if msg.WarnCount != wc {
+		t.Errorf("Got %d want %d", msg.WarnCount, wc)
+	}
+
+	if !bytes.Equal(msg.DashVars["-helo_name"], []byte(heloname)) {
+		t.Errorf("Got %q want %q", msg.DashVars["-helo_name"], heloname)
+	}
+
 	if msg.HdrFile != hf {
 		t.Errorf("Got %q want %q", msg.HdrFile, hf)
 	}
 
 	if msg.DtaFile != df {
 		t.Errorf("Got %q want %q", msg.DtaFile, df)
+	}
+}
+
+func TestErrors(t *testing.T) {
+	var p, id, emsg string
+	var err error
+	p = path.Join(gopath, "src/github.com/baruwa-enterprise/go-exim/testdata/1eXn2s-0008DG-EX-H")
+	id = "1eXn2s-0008DG-EX"
+	emsg = fmt.Sprintf(pathErr, p)
+	if _, err = NewMsg(p, id); err == nil {
+		t.Fatalf("An error should be returned")
+	}
+
+	if err.Error() != emsg {
+		t.Errorf("Got %q want %q", err, emsg)
+	}
+
+	id = "1cGaid-0000sq-9g"
+	p = path.Join(gopath, "src/github.com/baruwa-enterprise/go-exim/testdata")
+	emsg = fmt.Sprintf("stat %s/%s-H: no such file or directory", p, id)
+	if _, err = NewMsg(p, id); err == nil {
+		t.Fatalf("An error should be returned")
+	}
+
+	if err.Error() != emsg {
+		t.Errorf("Got %q want %q", err, emsg)
 	}
 }
